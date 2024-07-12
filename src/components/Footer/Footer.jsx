f5a681fb0c016b554925989497a55a119057deca
vishal086 <vishalkumar359877@gmail.com>
2024-07-12 10:35:22 +0530

    first commt

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <>
+    <div className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* left side */}
+        <div className="flexColStart f-left">
+          <h1 className="footer-name">Pratyush</h1>
+          <span>
+          Our vision is to provide the best homes <br />
+           to everyone associated with us.
+          </span>
+        </div>
+
+        <div className="flexColStart f-right">
+          <span className="footer-text">Information</span>
+          <span>MMMUT Gorakhpur , CSE Dept , 273010</span>
+        </div>
+      </div>
+    </div>
+
+    </>
+    
+  );
+};
+
+export default Footer;
